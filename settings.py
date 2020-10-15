13c24bcc633367a21c2b6db4481c8ec854fa8355
wscheffe <will.scheffe@gmail.com>
2020-10-15 10:51:17 -0500

    finally done

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 class Settings:
 
     def __init__(self):
-        #initialize the game's settings
+        #initialize the game's static settings
 
         # Screen Settings
         self.screen_width = 1200
@@ -10,10 +10,47 @@ class Settings:
 
         # ship settings
         self.ship_speed = 1.5
+        self.ship_limit = 3
 
         # bullet settings
-        self.bullet_speed = 1.0
+        self.bullet_speed = 2.0
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60,60,60)
         self.bullets_allowed = 3
+
+        # alien settings
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        # fleet direction of 1 represents right; -1 represents left
+        self.fleet_direction = 1
+
+        #how quickly the game speeds up
+        self.speedup_scale = 1.1
+
+        # how quickly the alien point values increase
+        self.score_scale = 1.5
+
+        self.initialize_dynamic_settings()
+
+    
+    def initialize_dynamic_settings(self):
+        """initialize game settings that change throughout the game"""
+        self.ship_speed = 1.5
+        self.bullet_speed = 3.0
+        self.alien_speed = 1.0
+
+        # fleet_dirction of 1 reprsents right; -1 represents left
+        self.fleet_direction = 1
+
+        #scoring
+        self.alien_points = 50
+
+    
+    def increase_speed(self):
+        """increase speed settings and alien point values"""
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points * self.score_scale)
